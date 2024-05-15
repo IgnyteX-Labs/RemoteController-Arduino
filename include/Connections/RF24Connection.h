08bb9a5bdeaa5507ea77957b584f453f95f16eef
toqix <64910316+toqix@users.noreply.github.com>
2024-05-15 11:51:50 +0200

    Implemented RF24Connection with the nrf24/RF24 library

--- include/Connections/RF24Connection.h
+++ include/Connections/RF24Connection.h
@@ -2,10 +2,11 @@
 #define RF24CONNECTION_H_
 
 #include "Connection.h"
+#include <RF24.h>
 
 class RF24Connection: public Connection {
 public:
-	RF24Connection();
+	// Connection class members
 	bool begin();
 	void end();
 	bool available();
@@ -13,6 +14,52 @@ public:
 	size_t getPayloadSize();
 	bool write(const void *buffer, size_t length);
 	size_t getMaxPackageSize();
+	
+	// RF24Connection specific members
+	/**
+	 * @brief Construct a new RF24Connection object
+	 *
+	 * @param cepin The pin attached to Chip Enable on the RF module
+	 * @param cspin The pin attached to Chip Select (often labeled CSN) on the radio module.
+	 */
+	RF24Connection(int cepin, int cspin);
+
+	/**
+	 * @brief Construct a new RF24Connection object
+	 * 
+	 * @param rf24 An already constructed and configured RF24 object
+	 */
+	RF24Connection(RF24 &rf24);
+
+	/**
+	 * @brief Construct a new RF24Connection object
+	 * 
+	 * @note Uses default SPI Bus and cs/ce Pins
+	 * 
+	 */
+	RF24Connection();
+
+	/**
+	 * @brief Allows to optionally specify a non-default SPI bus to use.
+	 * 
+	 * @warning This function must be called before RF24Connection::begin()
+	 * @warning Cannot be used together with RF24Connection::RF24Connection(RF24 &rf24)
+	 * 
+	 * @param spiBus A pointer or reference to an instantiated SPI bus object. The _SPI datatype is declared in the RF24 library to allow different SPI implementations.
+	 */
+	void useSpecificSPIBus(_SPI *spiBus);
+
+	/**
+	 * @brief The NRF24L01 module supports packages of size 4 bytes to 32 bytes.
+	 * 
+	 */
+	const size_t maxPackageSize = 32;
+
+private:
+	RF24 rf24;
+	_SPI *nonDefaultSPI = nullptr;
+	bool isRF24Initialized = false; // To not call rf24.begin if true
+	const uint8_t rf24_address[6];
 };
 
 #endif
--- src/Connections/RF24Connection.cpp
+++ src/Connections/RF24Connection.cpp
@@ -1,31 +1,87 @@
 #include "Connections/RF24Connection.h"
 
-RF24Connection::RF24Connection()
+RF24Connection::RF24Connection(int cepin, int cspin) : rf24(RF24(cepin, cspin)), rf24_address("RF000")
 {
+}
+
+RF24Connection::RF24Connection(RF24 &rf24) : rf24(rf24), rf24_address("RF000")
+{
+	isRF24Initialized = true;
+}
+
+RF24Connection::RF24Connection() : rf24(RF24()), rf24_address("RF000")
+{
+}
+
+void RF24Connection::useSpecificSPIBus(_SPI *spiBus)
+{
+	nonDefaultSPI = spiBus;
 }
 
 bool RF24Connection::begin()
 {
+	if (!isRF24Initialized)
+	{
+		// Connect to the NRF24L01 module
+		if (nonDefaultSPI)
+		{
+			// If a non-default SPI bus was specified use this one
+			if (!rf24.begin(nonDefaultSPI))
+				return false;
+		}
+		else
+		{
+			// Use default SPI bus
+			if (!rf24.begin())
+				return false;
+		}
+		// NRF24L01 configuration
+		rf24.setPALevel(RF24_PA_HIGH);
+
+		isRF24Initialized = true;
+	}
+	// Required NRF24L01 settings for RF24 to work
+	rf24.enableDynamicPayloads();
+	rf24.setAutoAck(true);
+	rf24.openReadingPipe(1, rf24_address);
+	rf24.startListening();
+	return true;
 }
 
 void RF24Connection::end()
 {
+	rf24.closeReadingPipe(1);
+	rf24.powerDown();
+	isRF24Initialized = false;
 }
+
 bool RF24Connection::available()
 {
+	return rf24.available();
 }
+
 void RF24Connection::read(void *buffer, size_t length)
 {
+	rf24.read(buffer, length);
 }
 
 size_t RF24Connection::getPayloadSize()
 {
+	return rf24.getDynamicPayloadSize();
 }
 
 bool RF24Connection::write(const void *buffer, size_t length)
 {
+	rf24.stopListening();
+	rf24.closeReadingPipe(1);
+	rf24.openWritingPipe(rf24_address);
+	bool success = rf24.write(buffer, length);
+	rf24.openReadingPipe(1, rf24_address);
+	rf24.startListening();
+	return success;
 }
 
-size_t RF24Connection::getMaxPackageSize() {
-	return 32;
+size_t RF24Connection::getMaxPackageSize()
+{
+	return maxPackageSize;
 }
